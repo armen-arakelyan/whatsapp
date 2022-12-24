656de67d4bda039a639dde4fbec225468e6586fe
armen-arakelyan <62097161+armen-arakelyan@users.noreply.github.com>
2022-12-24 17:19:27 +0400

    Delete build directory

--- build/logic.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const connection_1 = require("./connection");
-(0, connection_1.connect)().then(res => {
-    connection_1.ev.on('get labels', data => {
-        console.log('>>>', data);
-    });
-});
